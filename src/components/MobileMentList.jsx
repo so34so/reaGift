5353213f52f83f36ae9ae0acdbbd337dedd6f54d
so34so <koo3435@gmail.com>
2025-12-06 21:31:33 +0900

    모바일 변경

--- src/components/MobileMentList.jsx
+++ src/components/MobileMentList.jsx
@@ -5,28 +5,29 @@ import './MobileMentList.css';
 const MobileMentList = () => {
   return (
     <div className="mobile-ment-wrapper">
-      {/* 맨 위에 고정될 헤더 */}
+      {/* 맨 위 고정 헤더 */}
       <div className="mobile-ment-header">
         <h2>글 모음집</h2>
         <p>친구들이 남긴 축하 멘트들이에요 💌</p>
       </div>
 
-      {/* 아래 스크롤 가능한 댓글 리스트 */}
+      {/* 아래 스크롤 영역 */}
       <div className="mobile-ment-list">
         {exampleMentList.map((item) => (
           <div key={item.id} className="mobile-ment-item">
+            {/* 동그란 메인 이미지(이름 첫 글자) */}
             <div className="mobile-ment-avatar">
               {item.name?.[0] || '친'}
             </div>
-            <div className="mobile-ment-bubble">
+
+            {/* 오른쪽 내용 영역 */}
+            <div className="mobile-ment-body">
+              <div className="mobile-ment-name-row">
+                <span className="mobile-ment-name">{item.name}</span>
+                {/* 필요하면 여기에 @id 나 시간 같은 것도 추가 가능 */}
+              </div>
               <div className="mobile-ment-content">{item.content}</div>
-              <div className="mobile-ment-name">- {item.name}</div>
             </div>
           </div>
         ))}
-      </div>
-    </div>
-  );
-};
-
-export default MobileMentList;
+      </di
